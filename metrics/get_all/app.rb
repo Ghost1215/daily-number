6874785e2b45f58402bd751cd65139726903d35f
Ghost1215 <solugeniawollem@gmail.com>
2019-12-30 00:43:00 -0800

    added mauna loa co2

--- metrics/get_all/app.rb
+++ metrics/get_all/app.rb
@@ -3,119 +3,113 @@ require 'json'
 require 'nokogiri'
 require 'aws-sdk-dynamodb'
 require 'aws-sdk'
+require 'csv'
+require 'open-uri'
+require 'uuid'
 
 def lambda_handler(event:, context:)
-  
-  begin
-    
-    # Get page from Oilprice.com
-    response = HTTParty.get("https://oilprice.com/")
-    raise "Bad response from OilPrice" if response.code != 200
 
-    # parse values from it
-    page = Nokogiri::HTML(response)
+  begin
+    # unexpected that this should be required to be set, but fails when it isn't
+    # something about scoping in the blocks below
+    blend_name, value_from_oilprice, oilprice_metrics_array, item_id_array = false, false, [], []
+
+    uuid = UUID.new
+
+    client_params = {region: "us-west-2"}
+    client_params[:endpoint] = 'http://docker.for.mac.localhost:8000' unless ENV['AWS_SAM_LOCAL'].nil?
+    dynamodb = Aws::DynamoDB::Client.new(client_params)
+
+
+    # ############### Get Oilprice.com data
+
+    oilprice_response = HTTParty.get("https://oilprice.com/")
+    raise "Bad response from OilPrice" if oilprice_response.code != 200
+
+    page = Nokogiri::HTML(oilprice_response)
     table = page.search('table').first
-    table.search('tr').each do |row|
-      cells = row.search('td')
-      blend_name = cells.search('.blend_name').text.match(/^[^•]*/)[0].strip
-      
-      if blend_name.downcase == "wti crude"
-        value_from_oilprice = cells.search('.value').text
-        puts "WTI Crude: #{value_from_oilprice}"
-        
-        client_params = {region: "us-west-2"}
-        client_params[:endpoint] = 'http://docker.for.mac.localhost:8000' unless ENV['AWS_SAM_LOCAL'].nil?
-        dynamodb = Aws::DynamoDB::Client.new(client_params)
-        
-        # prepare row
+    table&.search('tr').each do |row|
+      cells = row&.search('td')
+      blend_name = cells&.search('.blend_name')&.text&.match(/^[^•]*/)[0]&.strip&.downcase
+
+      if !blend_name.nil?
+        value_from_oilprice = cells.search('.value').text.strip
+        if blend_name == "wti crude"
+          oilprice_metrics_array << ["WTI Crude", value_from_oilprice]
+        elsif blend_name == "brent crude"
+          oilprice_metrics_array << ["Brent Crude", value_from_oilprice]
+        elsif blend_name == "natural gas"
+          oilprice_metrics_array << ["Natural Gas", value_from_oilprice]
+        end
+      else
+        raise "failed to get blend_name"
+      end
+    end
+
+    ## Write Oilprice data to DynamoDB
+    if oilprice_metrics_array.size > 0
+      oilprice_metrics_array.each do |metric|
+        id = uuid.generate
         item = {
-          id: "#{Time.now.to_i}",
+          id: id,
           createdAt: "#{Time.now.strftime('%Y-%m-%dT%H:%M:%S.%L%z')}",
-          metric: 'WTI Crude',
-          value: value_from_oilprice,
+          metric: metric[0],
+          value: metric[1],
+          unit: "dollars",
         }
-        
         params = {
             table_name: 'MetricsSnapshotsTable',
-            item: item
+            item: item,
+            return_values: "NONE"
         }
-        
-        # Write metric value to table
-        dynamodb.put_item(params)          
+        resp = dynamodb.put_item(params)
+        item_id_array << id
       end
-    end # end table search
-    
-    
-    
-    
+    else
+      raise "failed to find oilprice data"
+    end
+
+    # ############### Get MLO data
+    mlo_csv = open('https://www.esrl.noaa.gov/gmd/webdata/ccgg/trends/co2_mlo_weekly.csv')
+    csva = CSV.parse(mlo_csv, :headers=>true).to_a
+    ppm = csva.last[1]
+    id = uuid.generate
+    mlo_item = {
+      id: id,
+      createdAt: "#{Time.now.strftime('%Y-%m-%dT%H:%M:%S.%L%z')}",
+      metric: "PPM CO2, Mauna Loa",
+      value: ppm,
+      unit: "ppm",
+    }
+    mlo_params = {
+        table_name: 'MetricsSnapshotsTable',
+        item: mlo_item,
+        return_values: "NONE"
+    }
+    resp = dynamodb.put_item(mlo_params)
+    item_id_array << id
+
+
+
+
+    # ######### Invoke Lambda to send email
+    prod = ENV['AWS_SAM_LOCAL'].nil? ? true : false
+
     lambda_client_params = {region: "us-west-2"}
-    lambda_client_params[:endpoint] = 'http://host.docker.internal:3001' unless ENV['AWS_SAM_LOCAL'].nil?
-
+    lambda_client_params[:endpoint] = 'http://host.docker.internal:3001' unless prod
     client = Aws::Lambda::Client.new(lambda_client_params)
-    req_payload = {:metricId => 1} # this should pass an array of hashes of metric id's and types
-    payload = JSON.generate(req_payload)
 
+    payload_hash = {:item_ids => item_id_array.join(",")} # this should pass an array of hashes of metric id's and types
+    payload = JSON.generate( payload_hash )
+
+    invoke_params = {
+      invocation_type: 'Event',
+      log_type: 'None', payload: payload
+    }
+    invoke_params[:function_name] = prod ? 'dailyn-SendEmailFunction-4T3P4J9XB670' : 'http://host.docker.internal:3001'
     # invocation_type: "Event - Invoke the function asynchronously. Send events that fail multiple times to the function's dead-letter queue (if it's configured). The API response only includes a status code."
-    resp = client.invoke({
-             function_name: 'dailyn-SendEmailFunction-4T3P4J9XB670',
-             invocation_type: 'Event',
-             log_type: 'None',
-             payload: payload
-           })
-    
-    
-    
-    
-    # # create separate lambda to do the send ######################
-    #
-    # # send email with value
-    # sender = "john@fnnny.com"
-    # recipient = "john@fnnny.com"
-    # awsregion = "us-west-2"
-    # subject = "Your daily numbers"
-    #
-    # # later: put this in its own lambda. have it get all the metrics collected that day, bundle them, and send them.
-    # htmlbody =
-    #   '<h1>Your daily numbers!</h1>'\
-    #   '<p>This email was sent with <a href="https://aws.amazon.com/ses/">sas</a>.'
-    # textbody = '<h1>Your daily numbers!</h1>'
-    # encoding = "UTF-8"
-    #
-    # # Create a new SES resource and specify a region
-    # ses = Aws::SES::Client.new(region: awsregion)
-    #
-    # resp = ses.send_email({
-    #   destination: {
-    #     to_addresses: [
-    #       recipient,
-    #     ],
-    #   },
-    #   message: {
-    #     body: {
-    #       html: {
-    #         charset: encoding,
-    #         data: htmlbody,
-    #       },
-    #       text: {
-    #         charset: encoding,
-    #         data: textbody,
-    #       },
-    #     },
-    #     subject: {
-    #       charset: encoding,
-    #       data: subject,
-    #     },
-    #   },
-    # source: sender,
-    # # Comment or remove the following line if you are not using
-    # # a configuration set
-    # # configuration_set_name: configsetname,
-    # })
-    # puts "Email sent!"
-    #
-    # # end separate lambda to do the send ######################
-      
-    
+    resp = client.invoke( invoke_params )
+
     {
       statusCode: 200,
       body: {
@@ -123,7 +117,7 @@ def lambda_handler(event:, context:)
         # location: response.body
       }.to_json
     }
-    
+
   rescue HTTParty::Error => e
     puts "HTTParty fail:"
     puts e.inspect
@@ -132,9 +126,6 @@ def lambda_handler(event:, context:)
     puts 'Unable to add metric:'
     puts e.inspect
     raise e
-  # rescue Aws::SES::Errors::ServiceError => e
-  #   puts "Email not sent. Error message: #{e}"
-  #   raise e
   rescue => e
     puts "Generic error:"
     puts e.inspect
--- metrics/send_email/app.rb
+++ metrics/send_email/app.rb
@@ -1,68 +1,41 @@
 require 'aws-sdk'
 
 def lambda_handler(event:, context:)
-  
+
   begin
-    
-    # # Get page from Oilprice.com
-    # response = HTTParty.get("https://oilprice.com/")
-    # raise "Bad response from OilPrice" if response.code != 200
-    #
-    # # parse values from it
-    # page = Nokogiri::HTML(response)
-    # table = page.search('table').first
-    # table.search('tr').each do |row|
-    #   cells = row.search('td')
-    #   blend_name = cells.search('.blend_name').text.match(/^[^•]*/)[0].strip
-    #
-    #   if blend_name.downcase == "wti crude"
-    #     value_from_oilprice = cells.search('.value').text
-    #     puts "WTI Crude: #{value_from_oilprice}"
-    #
-    #     # connect to DynamoDB
-    #     dynamodb = Aws::DynamoDB::Client.new(
-    #       region: "us-west-2",
-    #       endpoint: 'http://docker.for.mac.localhost:8000',
-    #     )
-    #
-    #     # prepare row
-    #     item = {
-    #       Id: "#{Time.now.to_i}",
-    #       CreatedAt: "#{Time.now.strftime('%Y-%m-%dT%H:%M:%S.%L%z')}",
-    #       metric: 'WTI Crude',
-    #       value: value_from_oilprice,
-    #     }
-    #
-    #     params = {
-    #         table_name: 'MetricsSnapshotsTable',
-    #         item: item
-    #     }
-    #
-    #     # Write metric value to table
-    #     dynamodb.put_item(params)
-    #   end
-    # end # end table search
-    
-    
-    
-    # create separate lambda to do the send ######################
-    
+
+    client_params = {region: "us-west-2"}
+    client_params[:endpoint] = 'http://docker.for.mac.localhost:8000' unless ENV['AWS_SAM_LOCAL'].nil?
+    dynamodb = Aws::DynamoDB::Client.new(client_params)
+
+    items = []
+    event['item_ids'].split(',').each do |item_id|
+      resp = dynamodb.get_item({
+        key: {
+          "id" => "#{item_id}",
+        },
+        table_name: "MetricsSnapshotsTable",
+      })
+      items << resp.item
+    end
+
     # send email with value
     sender = "john@fnnny.com"
     recipient = "john@fnnny.com"
     awsregion = "us-west-2"
-    subject = "Your daily numbers"
-    
+    subject = "Daily Numbers"
+
     # later: put this in its own lambda. have it get all the metrics collected that day, bundle them, and send them.
-    htmlbody =
-      '<h1>Your daily numbers!</h1>'\
-      '<p>This email was sent with <a href="https://aws.amazon.com/ses/">sas</a>.'
-    textbody = '<h1>Your daily numbers!</h1>'
+    htmlbody = '<h1>Your daily numbers</h1>'
+    items.each do |item|
+      htmlbody << "<div>#{item["metric"]}: #{(item['unit']=='dollars') ? '$' : ''}#{item["value"]}</a>"
+    # textbody = '<h1>Your daily numbers!</h1>'
+    end
     encoding = "UTF-8"
-    
+
     # Create a new SES resource and specify a region
     ses = Aws::SES::Client.new(region: awsregion)
-    
+
     resp = ses.send_email({
       destination: {
         to_addresses: [
@@ -75,10 +48,10 @@ def lambda_handler(event:, context:)
             charset: encoding,
             data: htmlbody,
           },
-          text: {
-            charset: encoding,
-            data: textbody,
-          },
+          # text: {
+          #   charset: encoding,
+          #   data: textbody,
+          # },
         },
         subject: {
           charset: encoding,
@@ -86,15 +59,12 @@ def lambda_handler(event:, context:)
         },
       },
     source: sender,
-    # Comment or remove the following line if you are not using 
+    # Comment or remove the following line if you are not using
     # a configuration set
     # configuration_set_name: configsetname,
     })
     puts "Email sent!"
-    
-    # end separate lambda to do the send ######################
-      
-    
+
     {
       statusCode: 200,
       body: {
@@ -102,7 +72,7 @@ def lambda_handler(event:, context:)
         # location: response.body
       }.to_json
     }
-    
+
   rescue Aws::SES::Errors::ServiceError => e
     puts "Email not sent. Error message: #{e}"
     raise e
